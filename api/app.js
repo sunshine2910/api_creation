86e44d706af2d238cd711be5995797c34c5a6aef
sunshine2910 <m_aufrechter@etu-webschoolfactory.fr>
2023-02-06 17:43:10 +0100

    tp1

--- api/app.js
+++ api/app.js
@@ -2,31 +2,53 @@
  * Express App
  */
 const express = require("express");
+const { defaults } = require("pg");
 const app = express();
 
 // middleware
-app.use(express.json());
+app.use(
+  express.json(),
+  negociate_format({
+    formats: ["application/json", "text/csv"],
+  })
+);
 
-app.response.render = function (users) {
-  this.format({
-    "application/json": () => this.json(users),
-    "text/plain": () => {
-      this.setHeader("Content-Type", "text/plain");
-      if (!Array.isArray(users)) users = [users];
-      this.send(users.map((u) => JSON.stringify(u)).join("\n"));
-    },
-    "text/csv": () => {
-      this.setHeader("Content-Type", "text/csv");
-      if (!Array.isArray(users)) users = [users];
-      this.write(Object.keys(users[0].dataValues).join(";") + "\n");
-      this.write(
-        users.map((u) => Object.values(u.dataValues).join(";")).join("\n")
-      );
-      this.end();
-    },
-    default: () => this.json(users),
-  });
-};
+function negociate_format({ formats }) {
+  return function (req, res, next) {
+    res.render = function (data) {
+      const func = {
+        "application/json": () => {
+          res.json(data);
+        },
+        "text/plain": () => {
+          res.setHeader("Content-Type", "text/plain");
+          if (!Array.isArray(data)) data = [data];
+          res.send(data.map((u) => JSON.stringify(u)).join("\n"));
+        },
+        "text/csv": () => {
+          res.setHeader("Content-Type", "text/csv");
+          if (!Array.isArray(data)) data = [data];
+          res.write(Object.keys(data[0].dataValues).join(";") + "\n");
+          res.write(
+            data.map((u) => Object.values(u.dataValues).join(";")).join("\n")
+          );
+          res.end();
+        },
+      };
+      const initialValue = {};
+      const sumWithInitial = formats.reduce((accumulator, currentValue) => {
+        accumulator[currentValue] = func[currentValue];
+        return accumulator;
+      }, initialValue);
+      sumWithInitial.default = function () {
+        // log the request and respond with 406
+        res.status(406).send('Not Acceptable')
+      }
+      res.format(sumWithInitial);
+    }
+    next();
+  };
+}
 
 // Routes
 app.use("/users", require("./routes/users"));
