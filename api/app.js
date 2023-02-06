390f60860d23909d802bfc7fa4698d3cde47f57a
Karl MARQUES <kmarques@vetixy.com>
2023-02-06 14:17:45 +0100

    fix: add content-type to response + manage single item

--- api/app.js
+++ api/app.js
@@ -10,8 +10,14 @@ app.use(express.json());
 app.response.render = function (users) {
   this.format({
     "application/json": () => this.json(users),
-    "text/plain": () => this.send(users.map((u) => u.email).join(",")),
+    "text/plain": () => {
+      this.setHeader("Content-Type", "text/plain");
+      if (!Array.isArray(users)) users = [users];
+      this.send(users.map((u) => JSON.stringify(u)).join("\n"));
+    },
     "text/csv": () => {
+      this.setHeader("Content-Type", "text/csv");
+      if (!Array.isArray(users)) users = [users];
       this.write(Object.keys(users[0].dataValues).join(";") + "\n");
       this.write(
         users.map((u) => Object.values(u.dataValues).join(";")).join("\n")
